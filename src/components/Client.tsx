07a31107f01ef9cfcd0517d92fec498898033db4
narawit <narawit.so@rmuti.ac.th>
2025-11-26 11:53:52 +0700

    first commit

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import Client01 from '../assets/client01.jpg'
+import Client02 from '../assets/client02.jpg'
+import Client03 from '../assets/client03.jpg'
+import Client04 from '../assets/client04.jpg'
+import Client05 from '../assets/client05.jpg'
+import Client06 from '../assets/client06.jpg'
+import Client07 from '../assets/client07.jpg'
+import Client08 from '../assets/client08.jpg'
+
 
 function Client() {
     return (
@@ -7,43 +16,43 @@ function Client() {
                 <h2 className='text-[25px] text-center '>ลูกค้าที่ไว้วางใจเรา </h2>
                 <div className='grid grid-cols-4 justify-between items-center gap-10'>
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client01} alt="" />
                         <h2 className='text-center mb-4'>SCGSM</h2>
                         <p>วิทยาลัยบัณฑิตศึกษาการจัดการ มหาวิทยาลัยขอนแก่น</p>
                     </div>
 
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client02} alt="" />
                         <h2 className='text-center mb-4'>สสว.</h2>
                         <p>สำนักงานส่งเสริมวิสาหกิจขนาดกลางและขนาดย่อม</p>
                     </div>
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client03} alt="" />
                         <h2 className='text-center mb-4'>Science Park KKU</h2>
                         <p>สำนักงานอุทยานวิทยาศาสตร์ มหาวิทยาลัยขอนแก่น</p>
                     </div>
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client04} alt="" />
                         <h2 className='text-center mb-4'>Khon Kaen Hotel</h2>
                         <p>โรงแรมขอนแก่นโฮเต็ล จังหวัดขอนแก่น</p>
                     </div>
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client05} alt="" />
                         <h2 className='text-center mb-4'>Regional Environmental Office</h2>
                         <p>สำนักงานสิ่งแวดล้อมภาคที่ 10 จังหวัดขอนแก่น</p>
                     </div>
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client06} alt="" />
                         <h2 className='text-center mb-4'>Department of Water Resources</h2>
                         <p>กรมทรัพยากรน้ำ กระทรวงทรัพยากรธรรมชาติและสิ่งแวดล้อม</p>
                     </div>
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client07} alt="" />
                         <h2 className='text-center mb-4'>Department of Highways</h2>
                         <p>กรมทางหลวงกระทรวงคมนาคม</p>
                     </div>
                     <div className='shadow-lg p-10 rounded-md '>
-                        <img className='object-cover w-[1000px] p-2' src='' alt="" />
+                        <img className='object-cover w-[1000px] p-2' src={Client08} alt="" />
                         <h2 className='text-center mb-4'>Security and traffic office KKU</h2>
                         <p>สำนักงานรักษาความปลอดภัยและการจราจร มหาวิทยาลัยขอนแก่น</p>
                     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,22 +24,25 @@ function Footer() {
     };
     return (
         <div>
-            <div className='flex flex-col gap-4 p-10 border-2 justify-between'>
+            <div className='flex flex-row  p-10  justify-center items-center gap-[100px] bg-[#f3f4f6]'>
                 <div className='flex flex-col gap-4'>
                     <h2>ชั้น 2 อาคารอุทยานวิทยาศาสตร์ มหาวิทยาลัยขอนแก่น </h2>
                     <h2>0942646926 </h2>
                     <h2>webmaster@esarn.com </h2>
                 </div>
-                <div className='flex flex-row gap-4'>
-                    <form onSubmit={sentMessage}>
+                <div >
+                    <form className='flex flex-col gap-4' onSubmit={sentMessage}>
                         <label htmlFor="">ชื่อของคุณ (ต้องการ)
                             <input type="text"
+                                className='border-2 rounded-md'
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
                                 name="" id="" />
                         </label>
                         <label htmlFor="">อีเมล์ของคุณ (ต้องการ)
                             <input type="email"
+                                className='border-2 rounded-md'
+
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 name="" id="" />
@@ -47,19 +50,24 @@ function Footer() {
 
                         <label htmlFor="">หัวข้อ
                             <input type="text"
+                                className='border-2 rounded-md'
+
                                 value={topic}
                                 onChange={(e) => setTopic(e.target.value)}
                                 name="" id="" />
                         </label>
 
                         <label htmlFor="">ข้อความของคุณ
-                            <input type="text"
+                            <textarea
+                                className='border-2 rounded-md'
                                 value={message}
                                 onChange={(e) => setMessage(e.target.value)}
                                 name="" id="" />
                         </label>
 
-                        <button type="submit">ส่ง</button>
+                        <button
+                            className='border-2 rounded-md'
+                            type="submit">ส่ง</button>
 
                     </form></div>
             </div>
